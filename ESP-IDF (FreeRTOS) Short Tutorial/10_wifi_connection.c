c1484b0f3bde5aa22d00e4178e330adbf35217ae
himanshu-singh-zhcet <himanshusingh.zhcet@gmail.com>
2025-02-13 22:03:36 +0530

    update

--- ESP-IDF (FreeRTOS) Short Tutorial/10_wifi_connection.c
+++ ESP-IDF (FreeRTOS) Short Tutorial/10_wifi_connection.c
@@ -6,7 +6,7 @@
 #include "nvs_flash.h"
 
 // Lets use our ESP board as an access point 
-#define AP_SSID      "ESPTEST"
+#define AP_SSID      "Nucleo5_7238"
 #define AP_PASSWORD  "12345678"
 #define AP_MAX_CONN  4
 #define AP_CHANNEL   0
@@ -14,30 +14,30 @@
 #define STA_SSID        "Redmi 8292"
 #define STA_PASSWORD    "12345678"
 
-void scann(){
-	wifi_scan_config_t scan_config = {
-		.ssid = 0,
-		.bssid = 0;
-		.channel = 0;
-		.show_hidden = true
-	};
-	printf("Start scanning \n");
-	ESP_ERROR_CHECK(esp_wifi_scan_start(&scan_config,true));
-	printf("Completed!\n");
-	uint8_t ap_num;
-	wifi_ap_record_t ap_records[20];
-	ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&ap_num,ap_records));
-    printf("Found %d access points: \n",ap_num);
-    printf("               SSID              | Channel | RSSI |   MAC \n\n");
-    for(int i = 0; i < ap_num; i++){
-    printf("%32s | %7d | %4d   %2x:%2x:%2x:%2x:%2x:%2x   \n", ap_records[i].ssid, ap_records[i].primary, ap_records[i].rssi , *ap_records[i].bssid, *(ap_records[i].bssid+1), *(ap_records[i].bssid+2), *(ap_records[i].bssid+3), *(ap_records[i].bssid+4), *(ap_records[i].bssid+5));
-    }
-}
+// void scann(){
+// 	wifi_scan_config_t scan_config = {
+// 		.ssid = 0,
+// 		.bssid = 0;
+// 		.channel = 0;
+// 		.show_hidden = true
+// 	};
+// 	printf("Start scanning \n");
+// 	ESP_ERROR_CHECK(esp_wifi_scan_start(&scan_config,true));
+// 	printf("Completed!\n");
+// 	uint8_t ap_num;
+// 	wifi_ap_record_t ap_records[20];
+// 	ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&ap_num,ap_records));
+//     printf("Found %d access points: \n",ap_num);
+//     printf("               SSID              | Channel | RSSI |   MAC \n\n");
+//     for(int i = 0; i < ap_num; i++){
+//     printf("%32s | %7d | %4d   %2x:%2x:%2x:%2x:%2x:%2x   \n", ap_records[i].ssid, ap_records[i].primary, ap_records[i].rssi , *ap_records[i].bssid, *(ap_records[i].bssid+1), *(ap_records[i].bssid+2), *(ap_records[i].bssid+3), *(ap_records[i].bssid+4), *(ap_records[i].bssid+5));
+//     }
+// }
 
 
 void app_main(){
 	ESP_ERROR_CHECK(nvs_flash_init());
-	tcpip_adapter_init();
+	// tcpip_adapter_init();
 
 	wifi_init_config_t wifi_config = WIFI_INIT_CONFIG_DEFAULT();
 	ESP_ERROR_CHECK(esp_wifi_init(&wifi_config));
@@ -69,7 +69,7 @@ void app_main(){
     ESP_ERROR_CHECK(esp_wifi_start());
     ESP_ERROR_CHECK(esp_wifi_connect());       */ 
 	while(1){
-		vTaskDelay(3000/portTICK_RATE_MS);
+		vTaskDelay(3000/ portTICK_PERIOD_MS);
 		// scann();
 	}
   
